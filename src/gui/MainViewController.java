c381f19d8f32dae339a55b0f060ded0ca0e5fbc6
Marcus Cezar <marcusferraz98@gmail.com>
2022-10-04 07:51:12 -0300

    Inicializing action as parameter in LoadView

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -16,6 +16,7 @@ import model.services.DepartmentService;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 public class MainViewController implements Initializable {
 
@@ -32,33 +33,17 @@ public class MainViewController implements Initializable {
     }
     @FXML
     public void onMenuItemDepartmentAction() {
-        loadViewTemp("/gui/DepartmentList.fxml");
+        loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+            controller.setDepartmentService(new DepartmentService());
+            controller.updateTableViewData();
+        });
     }
     @FXML
     public void onMenuItemAboutAction() {
-        loadView("/gui/About.fxml");
+        loadView("/gui/About.fxml", x -> {});
     }
 
-    private synchronized void loadView (String viewAbsoluteName) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(viewAbsoluteName));
-            VBox newVBox = loader.load();
-
-            Scene mainScene = Main.getMainScene();
-            VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-
-            Node mainMenu = mainVBox.getChildren().get(0);
-            mainVBox.getChildren().clear();
-            mainVBox.getChildren().add(mainMenu);
-            mainVBox.getChildren().addAll(newVBox.getChildren());
-        }
-        catch (IOException e) {
-            Alerts.showAlert("IOException", "Error loading view!", e.getMessage(), Alert.AlertType.ERROR);
-        }
-    }
-
-    //LoadView temporária para teste
-    private synchronized void loadViewTemp (String viewAbsoluteName) {
+    private synchronized <T> void loadView (String viewAbsoluteName, Consumer<T> initializingAction) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(viewAbsoluteName));
             VBox newVBox = loader.load();
@@ -71,15 +56,12 @@ public class MainViewController implements Initializable {
             mainVBox.getChildren().add(mainMenu);
             mainVBox.getChildren().addAll(newVBox.getChildren());
 
-            DepartmentListController controller = loader.getController();
-            controller.setDepartmentService(new DepartmentService());
-            controller.updateTableViewData();
+            initializingAction.accept(loader.getController());
         }
         catch (IOException e) {
             Alerts.showAlert("IOException", "Error loading view!", e.getMessage(), Alert.AlertType.ERROR);
         }
     }
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
